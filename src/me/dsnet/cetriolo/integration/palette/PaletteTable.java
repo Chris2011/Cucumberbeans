da917d00d060a7c1c40d94ffe0e80ade1ae57e00
Diego Zambelli Sessona <diego.sessona@gmail.com>
2012-12-20 17:52:25 +0000

    Added all basics palette action, need to create customizers

---
+++ src/me/dsnet/cetriolo/integration/palette/PaletteTable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.dsnet.cetriolo.integration.palette;
+
+/**
+ *
+ * @author SessonaD
+ */
+public class PaletteTable extends BasePaletteActiveEditor {
+
+    @Override
+    public String createBody() {
+        String body = "Scenario:";
+        return body;
+    }
+
+}
