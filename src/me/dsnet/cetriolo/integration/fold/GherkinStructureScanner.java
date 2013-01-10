e97ebb3d622baf386aeea4ba85d8ab37fecba5b9
Diego Zambelli Sessona <diego.sessona@gmail.com>
2013-01-10 11:35:32 +0000

    Code folding finished

--- src/me/dsnet/cetriolo/integration/fold/GherkinStructureScanner.java
+++ src/me/dsnet/cetriolo/integration/fold/GherkinStructureScanner.java
@@ -31,10 +31,8 @@ public class GherkinStructureScanner implements StructureScanner{
     }
 
     @Override
-    public Map<String, List<OffsetRange>> folds(ParserResult pr) {
-        System.out.println("############## in folds of the structure scanner");
+    public Map<String, List<OffsetRange>> folds(ParserResult pr) {        
         if (pr == null) {
-            System.out.println("############## parse result is null, returning empty map.");
             return Collections.emptyMap();
         }else{
             Map<String, List<OffsetRange>> folds = new HashMap<String, List<OffsetRange>>();
@@ -43,7 +41,8 @@ public class GherkinStructureScanner implements StructureScanner{
                 TokenHierarchy<GherkinTokenId> th = (TokenHierarchy<GherkinTokenId>) pr.getSnapshot().getTokenHierarchy();
                 TokenSequence<GherkinTokenId> ts = th.tokenSequence(GherkinTokenId.getLanguage());
                 List<OffsetRange> ranges = new ArrayList<OffsetRange>();
-                
+                List<OffsetRange> commentranges = new ArrayList<OffsetRange>();
+                //find starting points
                 List<Integer> startingBlocks = new ArrayList<Integer>();
                 while (ts.moveNext()) {
                     Token<GherkinTokenId> token = ts.token();
@@ -54,30 +53,65 @@ public class GherkinStructureScanner implements StructureScanner{
                                 break;
                             }
                         }
-                        startingBlocks.add(token.offset(th));                        
-                    }                       
+                        startingBlocks.add(token.offset(th));     
+                    //comments
+                    }else if (token.id().primaryCategory().equals("comment")){
+                        token = ts.token();
+                        int startcomment = token.offset(th);
+                        while(ts.moveNext()){
+                            token = ts.token();
+                            if(!token.id().primaryCategory().equals("comment")){
+                                ts.movePrevious();
+                                token = ts.token();
+                                commentranges.add(new OffsetRange(startcomment, token.offset(th) + token.length() -1));
+                                break;
+                            }
+                        }
+                    }                    
                 }
-                
-                
-                
+                //find end point for ranges and add the blocks
                 Integer[] blocks= startingBlocks.toArray(new Integer[0]);
                 for (int i = 0; i < blocks.length; i++) {
                     if(i  == (blocks.length-1)){
                         ranges.add(new OffsetRange(blocks[i], findEnd(ts, th) ));
                     }else{
-                        ranges.add(new OffsetRange(blocks[i], blocks[i] + 100));
+                        ranges.add(new OffsetRange(blocks[i], tuttISuccessiviAlPrimo(blocks[i+1],ts, th)));
                     }
                 }
-                
-                
-                
                 folds.put("codeblocks", ranges);
+                folds.put("comments", commentranges);
+                ts.moveStart();
+                
+            
             }catch(Exception e){
-                System.out.println("############## Exception in structure scanner");
                 e.printStackTrace();
             }
             return folds;
         }
+    }
+    
+    public int tuttISuccessiviAlPrimo(int offset,TokenSequence<GherkinTokenId> ts,TokenHierarchy<GherkinTokenId> th){
+        ts.move(offset -1);
+        Token<GherkinTokenId> token = ts.token();
+        //find first keyword
+        int trim=0;
+        while (ts.movePrevious()) {
+            token = ts.token();
+            if (token.id().primaryCategory().equals("keyword")) {
+                while (ts.movePrevious()){
+                    token = ts.token();
+                    if (!token.id().primaryCategory().equals("whitespace")  && !token.id().primaryCategory().equals("comment")) {
+                        token = ts.token();
+                        if (token.id().primaryCategory().equals("Table")){
+                            trim =1;
+                        }
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        return (token.offset(th) + token.length() - trim);
     }
     
     public int findEnd(TokenSequence<GherkinTokenId> ts,TokenHierarchy<GherkinTokenId> th){
@@ -86,25 +120,24 @@ public class GherkinStructureScanner implements StructureScanner{
         Token<GherkinTokenId> token = ts.token();
         while (ts.movePrevious()) {
             token = ts.token();
-            if (!token.id().primaryCategory().equals("whitespace")) {
+            if (!token.id().primaryCategory().equals("whitespace") && !token.id().primaryCategory().equals("comment")) {
                 break;
             }
         }
         return (token.offset(th) + token.length());
     }
 
-    public static void printToken(Token token){
-            System.out.println("\n\n### tok id:" + token.id());
-                    System.out.println("### tok name:" + token.id().name());
-                    System.out.println("### tok cat:" + token.id().primaryCategory());
-                    System.out.println("### tok ordinal:" + token.id().ordinal());
-                    System.out.println("### tok lenght:" + token.length()); 
-                    System.out.println("### tok lenght:" + token.text()); 
-        }
+//    public static void printToken(Token token) {
+//        System.out.println("\n\n### tok id:" + token.id());
+//        System.out.println("### tok name:" + token.id().name());
+//        System.out.println("### tok cat:" + token.id().primaryCategory());
+//        System.out.println("### tok ordinal:" + token.id().ordinal());
+//        System.out.println("### tok lenght:" + token.length());
+//        System.out.println("### tok lenght:" + token.text());
+//    }
     
     @Override
     public Configuration getConfiguration() {
-        System.out.println("############## here 2");
         return new Configuration(true, true);
     }
     
